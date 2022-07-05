6ad2f8f114e975f4ded9b6d6f0b6ed6003761523
cgil <lao.tseu.is.alive@gmail.com>
2022-07-05 10:49:28 +0200

    added S3 access policy to force https access

--- deploy_k8s_cluster/eks/storage.tf
+++ deploy_k8s_cluster/eks/storage.tf
@@ -15,3 +15,30 @@ resource "aws_s3_bucket_public_access_block" "state-bucket-public-access-block" 
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+# Deny all HTTP insecure request to the S3 bucket
+resource "aws_s3_bucket_policy" "goCompliantPolicyHttpsOnly" {
+  bucket = aws_s3_bucket.state.id
+
+  policy = jsonencode({
+    Version = "2022-07-7"
+    Id      = "goCompliantPolicyHttpsOnly"
+    Statement = [
+      {
+        Sid       = "HTTPSOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.state.arn,
+          "${aws_s3_bucket.state.arn}/*",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      },
+    ]
+  })
+}
