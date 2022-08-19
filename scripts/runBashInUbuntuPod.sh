2ee8f426af5b777bec3506aab8d6c96775660bb7
cgil <lao.tseu.is.alive@gmail.com>
2022-08-19 10:43:29 +0200

    included dedicated namespace creation

--- scripts/runBashInUbuntuPod.sh
+++ scripts/runBashInUbuntuPod.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "about to enter an interactive session that will end after you exit the bash terminal"
+
+# to get a list of pods via API
+# curl -s -k -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' $APISERVER/api/v1/pods |jq '.items[] | .metadata.namespace +":" + .metadata.name + ", ["  + .status.podIP + "]" + ", image:"  + .spec.containers[0].image + ", startTime:"  + .status.startTime '
 kubectl -n default run -i --tty bash --image=ubuntu --restart=Never -- bash
