a0a616208ef8b5b45d164d681429eb1d107ce22b
cgil <lao.tseu.is.alive@gmail.com>
2023-02-01 16:37:11 +0100

    using a fixed namespace for now

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ import (
 )
 
 const (
-	VERSION                = "0.4.8"
+	VERSION                = "0.4.9"
 	APP                    = "go-cloud-k8s-info"
 	defaultProtocol        = "http"
 	defaultPort            = 8080
