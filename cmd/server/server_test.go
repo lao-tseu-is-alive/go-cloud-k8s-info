a8c392312bb459b833d7fb52adabac5d833bbc6d
cgil <lao.tseu.is.alive@gmail.com>
2024-07-23 16:11:22 +0200

    reduced complexity TestMainExecution

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -39,6 +39,18 @@ type testStruct struct {
 	r              *http.Request
 }
 
+type TestMainStruct struct {
+	name                         string
+	contentType                  string
+	wantStatusCode               int
+	wantBody                     string
+	paramKeyValues               map[string]string
+	httpMethod                   string
+	url                          string
+	useFormUrlencodedContentType bool
+	body                         string
+}
+
 func printWantedReceived(wantBody string, receivedJson []byte) {
 	if DEBUG {
 		fmt.Printf("WANTED   :%T - %#v\n", wantBody, wantBody)
@@ -369,226 +381,6 @@ func TestGoHttpServerTimeHandler(t *testing.T) {
 	}
 }
 
-func TestGoHttpServerWaitHandler(t *testing.T) {
-	myServer := go_http.NewGoHttpServer(fmt.Sprintf(":%d", defaultPort), log.New(io.Discard, version.APP, 0))
-	ts := httptest.NewServer(myServer.GetWaitHandler(1))
-	defer ts.Close()
-	expectedResult := fmt.Sprintf("{\"waited\":\"%v seconds\"}", 1)
-
-	newRequest := func(method, url string, body string) *http.Request {
-		r, err := http.NewRequest(method, ts.URL+url, strings.NewReader(body))
-		if err != nil {
-			t.Fatalf(fmtErrNewRequest, method, url, err)
-		}
-		return r
-	}
-
-	tests := []testStruct{
-		{
-			name:           "1: Get on /wait should return Http Status Ok",
-			wantStatusCode: http.StatusOK,
-			wantBody:       expectedResult,
-			paramKeyValues: make(map[string]string),
-			r:              newRequest(http.MethodGet, "/wait", ""),
-		},
-		{
-			name:           "2: Post on /wait should return an http error method not allowed ",
-			wantStatusCode: http.StatusMethodNotAllowed,
-			wantBody:       "",
-			paramKeyValues: make(map[string]string),
-			r:              newRequest(http.MethodPost, "/wait", `{"task":"test not allowed method "}`),
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tt.r.Header.Set(go_http.HeaderContentType, go_http.MIMEAppJSONCharsetUTF8)
-			resp, err := http.DefaultClient.Do(tt.r)
-			l.Printf(fmtTraceInfo, tt.name, tt.r.Method, tt.r.URL)
-			defer go_http.CloseBody(resp.Body, tt.name, l)
-			if err != nil {
-				fmt.Printf(fmtErr, err, resp.Body)
-				t.Fatal(err)
-			}
-			assert.Equal(t, tt.wantStatusCode, resp.StatusCode, assertCorrectStatusCodeExpected)
-			receivedJson, _ := io.ReadAll(resp.Body)
-
-			printWantedReceived(tt.wantBody, receivedJson)
-			// check that receivedJson contains the specified tt.wantBody substring . https://pkg.go.dev/github.com/stretchr/testify/assert#Contains
-			assert.Contains(t, string(receivedJson), tt.wantBody, msgRespNotExpected)
-		})
-	}
-}
-
-func TestMainExecution(t *testing.T) {
-	defaultPort := 9999
-	listenAddr := fmt.Sprintf("%s://%s:%d", "http", "127.0.0.1", defaultPort)
-	err := os.Setenv("PORT", fmt.Sprintf("%d", defaultPort))
-	if err != nil {
-		t.Errorf("💥💥 ERROR: Unable to set env variable PORT")
-		return
-	}
-	fmt.Printf("INFO: 'Will start HTTP server listening on port %s'\n", listenAddr)
-	// starting main in his own go routine
-	var wg sync.WaitGroup
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		main()
-	}()
-	go_http.WaitForHttpServer(listenAddr, 1*time.Second, 10)
-
-	newRequest := func(method, url string, body string, useFormUrlencodedContentType bool) *http.Request {
-		fmt.Printf("INFO: 🚀🚀'newRequest %s on %s ##BODY : %+v'\n", method, url, body)
-		r, err := http.NewRequest(method, url, strings.NewReader(body))
-		if err != nil {
-			t.Fatalf(fmtErrNewRequest, method, url, err)
-		}
-		if method == http.MethodPost && useFormUrlencodedContentType {
-			r.Header.Set(go_http.HeaderContentType, "Application/x-www-form-urlencoded")
-		} else {
-			r.Header.Set(go_http.HeaderContentType, go_http.MIMEAppJSON)
-		}
-		return r
-	}
-
-	type testStruct struct {
-		name                         string
-		contentType                  string
-		wantStatusCode               int
-		wantBody                     string
-		paramKeyValues               map[string]string
-		httpMethod                   string
-		url                          string
-		useFormUrlencodedContentType bool
-		body                         string
-	}
-
-	tests := []testStruct{
-		{
-			name:                         "Get on default get handler should contain the Appname field",
-			wantStatusCode:               http.StatusOK,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     fmt.Sprintf("\"appname\": \"%s\"", version.APP),
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodGet,
-			url:                          "/",
-			useFormUrlencodedContentType: false,
-			body:                         "",
-		},
-		{
-			name:                         "Post on default get handler should return an http error method not allowed ",
-			wantStatusCode:               http.StatusMethodNotAllowed,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     "Method Not Allowed",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodPost,
-			url:                          "/",
-			useFormUrlencodedContentType: true,
-			body:                         `{"junk":"test with junk text"}`,
-		},
-		{
-			name:                         "Get on nonexistent route should return an http error not found ",
-			wantStatusCode:               http.StatusNotFound,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     "page not found",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodGet,
-			url:                          "/aroutethatwillneverexisthere",
-			useFormUrlencodedContentType: false,
-			body:                         "",
-		},
-		{
-			name:                         "Get on default Server Path should return a valid json containing param value",
-			wantStatusCode:               http.StatusOK,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     `"param_name": "╚»☯💥⚡✌ℂ𝔾𝕀𝕃✌⚡💥☯«╝"`,
-			paramKeyValues:               map[string]string{"name": "╚»☯💥⚡✌ℂ𝔾𝕀𝕃✌⚡💥☯«╝"},
-			httpMethod:                   http.MethodGet,
-			url:                          "/",
-			useFormUrlencodedContentType: false,
-			body:                         "",
-		},
-		{
-			name:                         "/health Post should return an http error method not allowed ",
-			wantStatusCode:               http.StatusMethodNotAllowed,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     "",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodPost,
-			url:                          "/health",
-			useFormUrlencodedContentType: false,
-			body:                         `{"task":"test not allowed method "}`,
-		},
-		{
-			name:                         "/readiness Post should return an http error method not allowed ",
-			wantStatusCode:               http.StatusMethodNotAllowed,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     "",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodPost,
-			url:                          "/readiness",
-			useFormUrlencodedContentType: false,
-			body:                         `{"task":"test not allowed method "}`,
-		},
-		{
-			name:                         "/time Post should return an http error method not allowed ",
-			wantStatusCode:               http.StatusMethodNotAllowed,
-			contentType:                  go_http.MIMEAppJSON,
-			wantBody:                     "",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodPost,
-			url:                          "/time",
-			useFormUrlencodedContentType: false,
-			body:                         `{"task":"test not allowed method "}`,
-		},
-		{
-			name:                         "/hello Get should return a welcome message",
-			wantStatusCode:               http.StatusOK,
-			contentType:                  go_http.MIMEHtml,
-			wantBody:                     "Hello World!",
-			paramKeyValues:               make(map[string]string),
-			httpMethod:                   http.MethodGet,
-			url:                          "/hello",
-			useFormUrlencodedContentType: false,
-			body:                         ``,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// prepare the request for this test case
-			r := newRequest(tt.httpMethod, listenAddr+tt.url, tt.body, tt.useFormUrlencodedContentType)
-			if len(tt.paramKeyValues) > 0 {
-				parameters := r.URL.Query()
-				for paramName, paramValue := range tt.paramKeyValues {
-					parameters.Add(paramName, paramValue)
-				}
-				r.URL.RawQuery = parameters.Encode()
-			}
-			l.Printf(fmtTraceInfo, tt.name, tt.httpMethod, tt.url)
-			resp, err := http.DefaultClient.Do(r)
-			if err != nil {
-				fmt.Printf(fmtErr, err, resp.Body)
-				t.Fatal(err)
-			}
-			defer go_http.CloseBody(resp.Body, tt.name, l)
-			assert.Equal(t, tt.wantStatusCode, resp.StatusCode, assertCorrectStatusCodeExpected)
-			receivedJson, _ := io.ReadAll(resp.Body)
-			rInfo := &info.RuntimeInfo{}
-			printWantedReceived(tt.wantBody, receivedJson)
-			if tt.wantStatusCode == http.StatusOK {
-				if tt.contentType == go_http.MIMEAppJSON {
-					err = json.Unmarshal(receivedJson, rInfo)
-					assert.Nil(t, err, "the output should be a valid json")
-				}
-			}
-			// check that receivedJson contains the specified tt.wantBody substring . https://pkg.go.dev/github.com/stretchr/testify/assert#Contains
-			assert.Contains(t, string(receivedJson), tt.wantBody, msgRespNotExpected)
-		})
-	}
-}
-
 func TestGetKubernetesConnInfo(t *testing.T) {
 
 	l := log.New(os.Stdout, version.APP, log.Lshortfile)
@@ -763,6 +555,226 @@ func TestGetJsonFromUrl(t *testing.T) {
 			}
 			assert.Equalf(t, tt.want, got, "GetJsonFromUrl(%v, %v, %v, %v)", tt.args.url, tt.args.bearerToken, tt.args.caCert, tt.args.logger)
 		})
+	}
+}
+
+func TestGoHttpServerWaitHandler(t *testing.T) {
+	myServer := go_http.NewGoHttpServer(fmt.Sprintf(":%d", defaultPort), log.New(io.Discard, version.APP, 0))
+	ts := httptest.NewServer(myServer.GetWaitHandler(1))
+	defer ts.Close()
+	expectedResult := fmt.Sprintf("{\"waited\":\"%v seconds\"}", 1)
+
+	newRequest := func(method, url string, body string) *http.Request {
+		r, err := http.NewRequest(method, ts.URL+url, strings.NewReader(body))
+		if err != nil {
+			t.Fatalf(fmtErrNewRequest, method, url, err)
+		}
+		return r
+	}
+
+	tests := []testStruct{
+		{
+			name:           "1: Get on /wait should return Http Status Ok",
+			wantStatusCode: http.StatusOK,
+			wantBody:       expectedResult,
+			paramKeyValues: make(map[string]string),
+			r:              newRequest(http.MethodGet, "/wait", ""),
+		},
+		{
+			name:           "2: Post on /wait should return an http error method not allowed ",
+			wantStatusCode: http.StatusMethodNotAllowed,
+			wantBody:       "",
+			paramKeyValues: make(map[string]string),
+			r:              newRequest(http.MethodPost, "/wait", `{"task":"test not allowed method "}`),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.r.Header.Set(go_http.HeaderContentType, go_http.MIMEAppJSONCharsetUTF8)
+			resp, err := http.DefaultClient.Do(tt.r)
+			l.Printf(fmtTraceInfo, tt.name, tt.r.Method, tt.r.URL)
+			defer go_http.CloseBody(resp.Body, tt.name, l)
+			if err != nil {
+				fmt.Printf(fmtErr, err, resp.Body)
+				t.Fatal(err)
+			}
+			assert.Equal(t, tt.wantStatusCode, resp.StatusCode, assertCorrectStatusCodeExpected)
+			receivedJson, _ := io.ReadAll(resp.Body)
+
+			printWantedReceived(tt.wantBody, receivedJson)
+			// check that receivedJson contains the specified tt.wantBody substring . https://pkg.go.dev/github.com/stretchr/testify/assert#Contains
+			assert.Contains(t, string(receivedJson), tt.wantBody, msgRespNotExpected)
+		})
+	}
+}
+
+func setPortEnv(t *testing.T, port int) {
+	err := os.Setenv("PORT", fmt.Sprintf("%d", port))
+	if err != nil {
+		t.Errorf("💥💥 ERROR: Unable to set env variable PORT")
+		t.FailNow()
+	}
+}
+
+func startMainServer(wg *sync.WaitGroup) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		main()
+	}()
+}
+
+func newRequest(t *testing.T, method, url, body string, useFormUrlencodedContentType bool) *http.Request {
+	fmt.Printf("INFO: 🚀🚀'newRequest %s on %s ##BODY : %+v'\n", method, url, body)
+	r, err := http.NewRequest(method, url, strings.NewReader(body))
+	if err != nil {
+		t.Fatalf(fmtErrNewRequest, method, url, err)
+	}
+	if method == http.MethodPost && useFormUrlencodedContentType {
+		r.Header.Set(go_http.HeaderContentType, "Application/x-www-form-urlencoded")
+	} else {
+		r.Header.Set(go_http.HeaderContentType, go_http.MIMEAppJSON)
+	}
+	return r
+}
+
+func executeTest(t *testing.T, tt TestMainStruct, listenAddr string, l *log.Logger) {
+	t.Run(tt.name, func(t *testing.T) {
+		// prepare the request for this test case
+		r := newRequest(t, tt.httpMethod, listenAddr+tt.url, tt.body, tt.useFormUrlencodedContentType)
+		if len(tt.paramKeyValues) > 0 {
+			parameters := r.URL.Query()
+			for paramName, paramValue := range tt.paramKeyValues {
+				parameters.Add(paramName, paramValue)
+			}
+			r.URL.RawQuery = parameters.Encode()
+		}
+		l.Printf(fmtTraceInfo, tt.name, tt.httpMethod, tt.url)
+		resp, err := http.DefaultClient.Do(r)
+		if err != nil {
+			fmt.Printf(fmtErr, err, resp.Body)
+			t.Fatal(err)
+		}
+		defer go_http.CloseBody(resp.Body, tt.name, l)
+		assert.Equal(t, tt.wantStatusCode, resp.StatusCode, assertCorrectStatusCodeExpected)
+		receivedJson, _ := io.ReadAll(resp.Body)
+		rInfo := &info.RuntimeInfo{}
+		printWantedReceived(tt.wantBody, receivedJson)
+		if tt.wantStatusCode == http.StatusOK {
+			if tt.contentType == go_http.MIMEAppJSON {
+				err = json.Unmarshal(receivedJson, rInfo)
+				assert.Nil(t, err, "the output should be a valid json")
+			}
+		}
+		// check that receivedJson contains the specified tt.wantBody substring
+		assert.Contains(t, string(receivedJson), tt.wantBody, msgRespNotExpected)
+	})
+}
+
+func TestMainExecution(t *testing.T) {
+	defaultPort := 9999
+	listenAddr := fmt.Sprintf("%s://%s:%d", "http", "127.0.0.1", defaultPort)
+	setPortEnv(t, defaultPort)
+	fmt.Printf("INFO: 'Will start HTTP server listening on port %s'\n", listenAddr)
+	// starting main in his own go routine
+	var wg sync.WaitGroup
+	startMainServer(&wg)
+	go_http.WaitForHttpServer(listenAddr, 1*time.Second, 10)
+
+	tests := []TestMainStruct{
+		{
+			name:                         "Get on default get handler should contain the Appname field",
+			wantStatusCode:               http.StatusOK,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     fmt.Sprintf("\"appname\": \"%s\"", version.APP),
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodGet,
+			url:                          "/",
+			useFormUrlencodedContentType: false,
+			body:                         "",
+		},
+		{
+			name:                         "Post on default get handler should return an http error method not allowed ",
+			wantStatusCode:               http.StatusMethodNotAllowed,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     "Method Not Allowed",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodPost,
+			url:                          "/",
+			useFormUrlencodedContentType: true,
+			body:                         `{"junk":"test with junk text"}`,
+		},
+		{
+			name:                         "Get on nonexistent route should return an http error not found ",
+			wantStatusCode:               http.StatusNotFound,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     "page not found",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodGet,
+			url:                          "/aroutethatwillneverexisthere",
+			useFormUrlencodedContentType: false,
+			body:                         "",
+		},
+		{
+			name:                         "Get on default Server Path should return a valid json containing param value",
+			wantStatusCode:               http.StatusOK,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     `"param_name": "╚»☯💥⚡✌ℂ𝔾𝕀𝕃✌⚡💥☯«╝"`,
+			paramKeyValues:               map[string]string{"name": "╚»☯💥⚡✌ℂ𝔾𝕀𝕃✌⚡💥☯«╝"},
+			httpMethod:                   http.MethodGet,
+			url:                          "/",
+			useFormUrlencodedContentType: false,
+			body:                         "",
+		},
+		{
+			name:                         "/health Post should return an http error method not allowed ",
+			wantStatusCode:               http.StatusMethodNotAllowed,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     "",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodPost,
+			url:                          "/health",
+			useFormUrlencodedContentType: false,
+			body:                         `{"task":"test not allowed method "}`,
+		},
+		{
+			name:                         "/readiness Post should return an http error method not allowed ",
+			wantStatusCode:               http.StatusMethodNotAllowed,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     "",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodPost,
+			url:                          "/readiness",
+			useFormUrlencodedContentType: false,
+			body:                         `{"task":"test not allowed method "}`,
+		},
+		{
+			name:                         "/time Post should return an http error method not allowed ",
+			wantStatusCode:               http.StatusMethodNotAllowed,
+			contentType:                  go_http.MIMEAppJSON,
+			wantBody:                     "",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodPost,
+			url:                          "/time",
+			useFormUrlencodedContentType: false,
+			body:                         `{"task":"test not allowed method "}`,
+		},
+		{
+			name:                         "/hello Get should return a welcome message",
+			wantStatusCode:               http.StatusOK,
+			contentType:                  go_http.MIMEHtml,
+			wantBody:                     "Hello World!",
+			paramKeyValues:               make(map[string]string),
+			httpMethod:                   http.MethodGet,
+			url:                          "/hello",
+			useFormUrlencodedContentType: false,
+			body:                         ``,
+		},
+	}
+
+	for _, tt := range tests {
+		executeTest(t, tt, listenAddr, l)
 	}
 }
 
